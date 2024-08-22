eaca34c6822531e65e3c2fdb7bfa98d008e99107
Dan Marino <technicaldanny302@gmail.com>
2024-08-22 10:12:17 -0700

    completed exercise

---
+++ FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+public string FizzBuzz(int number)
+{
+    var answer = "";
+    if(number % 3 == 0)
+    {
+        answer = "fizz";
+    }
+    if( number % 5 == 0)
+    {
+        answer += "Buzz";
+    }
+    return answer;
+}
